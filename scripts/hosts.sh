6cc9c1adc5ca77834f3c8d1f30c394fc7310ae9c
Tuxliban Torvalds <tenshalito@gmail.com>
2018-12-31 19:16:42 -0600

    Nuevo script agregado al directorio 'scripts'

---
+++ scripts/hosts.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Script para descargar actualizaciones de lista de direcciones hosts
+
+wget -O /etc/hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts
