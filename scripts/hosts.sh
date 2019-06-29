a906417282576c53694d83696360e23bb5bd7139
Tuxliban Torvalds <tenshalito@gmail.com>
2019-06-29 03:41:31 -0500

    Actualización de versión del script
    Se han añadido nuevas tareas en el script entre las cuales destacan:
    1. Realizar una copia de seguridad del archivo original
    2. Descargar una lista actualizada del fichero desde un repositorio remoto
    3. Se agrega un parche con direcciones personalizadas recopiladas por mi (esto podría ser opcional)
    4. Notificación de actualización del fichero hosts

--- scripts/hosts.sh
+++ scripts/hosts.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-# Script para descargar actualizaciones de lista de direcciones hosts
+# V0.3
+# Script para descargar una lista personalizada con direcciones para bloquearlas 
+# a través del fichero hosts
 
-wget -O /etc/hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts
+# Para automatizar este proceso se recomienda crear una tarea (crontab) y ajustarla a las necesidades del usuario (diario, semanal
+# mensual, etc)
+
+# Realizar copia de seguridad del fichero hosts previo
+echo -e "\e[34mHaciendo copia de seguridad del fichero hosts...\e[0m"; cp /etc/hosts /etc/hosts.bak &&  #sleep 1s; echo -e "\e[32mOK\e[0m"
+
+# Descargar lista mas reciente del repositorio y copiarlo al fichero hosts
+echo -e "\e[93mDescargando y copiando lista actualizada para fichero hosts...\e[0m"; wget -O /etc/hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts && #sleep 1s; echo -e "\e[32mOK\e[0m" 
+
+# Agregando lista personalizada de páginas al fichero hosts
+echo -e "\e[31mAgregando parche de lista personalizada al fichero hosts...\e[0m"; cat /home/skynet/Datos/Git_Hub/Void_Linux/otros/parche >> /etc/hosts; sv restart NetworkManager &&
+
+# Notificacion de actualizacion del fichero
+echo -e "\e[96mTarea finalizada. Fichero host actualizado\e[0m"; notify-send -t 5000 -i ~/.icons/hosts-update.png "Tarea finalizada" 'Fichero hosts actulizado'
