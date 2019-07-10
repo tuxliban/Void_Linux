d57a5aae94af9851b19c66476e29d198795a76fa
Tuxliban Torvalds <tenshalito@gmail.com>
2019-07-09 23:56:38 -0500

    Script para la bateria 1er intento en hacer un script para notificaciones de la bateria
    Este es el primer intento en crear un script que muestre notificaciones
    cuando quede poca carga de la bateria de una computadora portatil. La
    idea es que muestre una notificación cuando la carga de la bateria esté
    al 20% y una segunda notificación cuando la carga sean igual o menor al
    15 % en la que la computadora entre modo de ahorro de energía

---
+++ scripts/bateria.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+porcentaje=$(cat /sys/class/power_supply/BAT1/capacity)
+restante=$(acpi | awk '{print $5}')
+estado=$(acpi | awk '{print $3}' | cut -d"," -f1)
+
+# Comprobar si el porcentaje de la bateria es mayor a 20%
+if [ "$porcentaje" -gt 20 ]; then
+	echo ""
+
+# En caso de que el porcentaje de batería sea menor o igual a 20%, mostrar el siguiente mensaje
+elif
+	[ "$porcentaje" -eq 20 ]; then
+	notify-send --urgency=normal "Conectar cargador" "Tiempo de bateria disponible $restante"
+
+# En caso de que el porcentaje sea menor o igual al 15% y la bateria se está descargando, poner en modo "Suspensión" el equipo
+else
+	if
+#		[ "$porccentaje" -le 15 && 'Discharging' == "$estado" ]; then
+		[ "$porcentaje" -le 15 ]; then
+		notify-send --urgency=critical "Equipo entrará en modo 'Suspender' en 20 segundos..."
+		sleep 20 && sudo zzz
+	else
+		[ "$estado" = 'Charging' ]
+		notify-send --urgency=normal "Cargando bateria..."
+	fi
+fi
