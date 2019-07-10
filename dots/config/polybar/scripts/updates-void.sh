bfcf5230a4c9903703015c3792303eea994a6b97
Tuxliban Torvalds <tenshalito@gmail.com>
2019-07-09 23:24:40 -0500

    Actualizacion de script El principal cambio muestra las actulizaciones disponibles en un popup
    Ahora el script ademas de mostrar el total de actualizaciones en el
    panel, tambien muestra los paquetes disponibles por actualizar mediante
    una popup a traves de notify-send

--- dots/config/polybar/scripts/updates-void.sh
+++ dots/config/polybar/scripts/updates-void.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-if ! updates=$(sudo xbps-install -Su --dry-run 2> /dev/null | wc -l); then
+lista=$(xbps-install -nu | awk '{print $1}')
+
+if ! updates=$(xbps-install -nu | wc -l); then
 	updates=0
 fi
 
 if [ "$updates" -gt 0 ]; then
-	notify-send -i "/home/skynet/.config/polybar/scripts/updates.png" "Hay $updates actualizaciones disponibles"
+	notify-send -i "/home/skynet/.config/polybar/scripts/updates.png" "Actualizaciones disponibles:" "$lista"
 	echo "$updates"
 else
 	echo ""
